d8a828ec845fb2ca722074863715604bc5cfacc3
haganta-g <50399914+haganta-g@users.noreply.github.com>
2026-06-24 23:21:31 +0700

    Changed email formatting.

--- utils/email_utils.py
+++ utils/email_utils.py
@@ -12,15 +12,27 @@ def construct_email() -> str:
         <html>
             <head></head>
             <body>
+                <h1>Stock Exchange Daily Webscraper</h1>\n
                 <p>Please find below the relevant articles found today:<br>\n
         """
         reader: csv.DictReader = csv.DictReader(csvfile, fieldnames=csv_headers)
+        site_dict: dict = dict()
         for row in reader:
             if row["keywords"] == "":
                 continue
             retrieval_date: datetime = parser.parse(row["retrieved_at"])
             if retrieval_date.date() == datetime.today().date():
-                email_html += f"<a href=\"{row["link"]}\">{row["title"]}</a> - found keyword(s) {row["keywords"]}<br>\n"
+                site_name: str = row["link"].split("//")[1].split("/")[0]
+                data: list = [row["link"], row["title"], row["date"], row["keywords"]]
+                if site_name not in site_dict.keys():
+                    site_dict[site_name] = [data]
+                else:
+                    site_dict[site_name].append(data)
+            ...
+        for sites in site_dict.keys():
+            email_html += f"<h2>{sites}</h2>"
+            for site_data in site_dict[sites]:
+                email_html += f"<a href=\"{site_data[0]}\">{site_data[1]}</a> - found keyword(s) {site_data[3]}<br>\n"
         email_html += """
                 </p>
             </body>
