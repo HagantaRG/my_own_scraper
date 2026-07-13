a50a88dc510d6c3f3bfabe559a6e7f31968aed95
haganta-g <50399914+haganta-g@users.noreply.github.com>
2026-07-13 10:58:20 +0100

    Refactored project.

---
+++ src/utils/filepaths.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+
+PROJECT_FOLDER: Path = Path(__file__).parent.parent.parent
+LOGS_FOLDER: Path = Path(f"{PROJECT_FOLDER}/logs")
+SETTINGS_FOLDER: Path = Path(f"{PROJECT_FOLDER}/settings")
+DATA_FOLDER: Path = Path(f"{PROJECT_FOLDER}/data")
+SOURCE_FOLDER: Path = Path(f"{PROJECT_FOLDER}/src")
--- utils/filepaths.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-PYTHON_FOLDER: Path = Path(__file__).parent.parent
-LOGS_FOLDER: Path = Path(f"{PYTHON_FOLDER}/logs")
-SETTINGS_FOLDER: Path = Path(f"{PYTHON_FOLDER}/settings")
-DATA_FOLDER: Path = Path(f"{PYTHON_FOLDER}/data")
