d60fefe050c093f17c6abc5366f4483aaf970e28
haganta-g <50399914+haganta-g@users.noreply.github.com>
2026-07-18 11:54:14 +0100

    Removed unused function.

--- src/utils/news_utils.py
+++ src/utils/news_utils.py
@@ -25,17 +25,3 @@ class NewsInformation:
         self.relevant_keywords = relevant_keywords
         self.retrieved_at = retrieved_at
         self.news_location = news_location if news_location is not ... else None
-
-def notify(info: NewsInformation) -> None:
-    """
-    Notifies relevant parties via WhatsApp business API messages. Ideally. :)
-    For now just print stuff lol.
-    """
-    info_string: str = (f"{info.news_link}\n"
-                        f"{info.news_title}\n"
-                        f"{info.news_date}\n"
-                        f"{info.retrieved_at}\n"
-                        f"{info.relevant_keywords}\n")
-    if info.news_location is not None:
-        info_string = f"{info_string}\n keywords located at page(s) {info.news_location}"
-    print(info_string)
